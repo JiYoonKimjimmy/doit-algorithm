77dccbc8c7620e43c306855bc095303a2df9b612
jimmyberg <jimmyberg.kim@gmail.com>
2023-01-09 10:21:36 +0900

    PrintUtil 추가
    - integer 배열 print 공통 함수 추가

---
+++ src/main/java/util/PrintUtil.java
@@ -0,0 +1,12 @@
+package util;
+
+public class PrintUtil {
+
+    public static void print(int[] A) {
+        for (int a: A) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+
+}
---
+++ src/test/java/util/PrintUtilTest.java
@@ -0,0 +1,13 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+
+public class PrintUtilTest {
+
+    @Test
+    public void int형_배열_테스트() {
+        int[] A = {1, 2, 3, 4, 5};
+        PrintUtil.print(A);
+    }
+
+}
